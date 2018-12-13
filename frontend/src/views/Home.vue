7270d96a276af4c7f0843653e9289c270bc83ea2
danilo.madrigalejos <danilo.madrigalejos@amdatex.com>
2018-12-13 15:55:42 +0800

    Removed helloworld component and about view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,4 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+  </div>  
 </template>
-
-<script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
-</script>
